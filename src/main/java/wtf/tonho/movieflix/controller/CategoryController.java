4355158b5f8e1b44a19545c75f3dc1e8d42aa347
Tonho <antonioreverso.com@gmail.com>
2025-04-20 20:09:19 -0300

    feat: create findall

--- src/main/java/wtf/tonho/movieflix/controller/CategoryController.java
+++ src/main/java/wtf/tonho/movieflix/controller/CategoryController.java
@@ -1,12 +1,28 @@
 package wtf.tonho.movieflix.controller;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import wtf.tonho.movieflix.entity.Category;
+import wtf.tonho.movieflix.repository.CategoryRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movieflix/category")
+@RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+
+    @GetMapping()
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
 
 }
--- src/main/java/wtf/tonho/movieflix/service/CategoryService.java
+++ src/main/java/wtf/tonho/movieflix/service/CategoryService.java
@@ -1,8 +1,19 @@
 package wtf.tonho.movieflix.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import wtf.tonho.movieflix.entity.Category;
+import wtf.tonho.movieflix.repository.CategoryRepository;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
 
+    private final CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
 }
