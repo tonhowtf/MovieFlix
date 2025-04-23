697c683e787ab5db0d96c2bb102ca2e9c7b66969
Tonho <antonioreverso.com@gmail.com>
2025-04-23 19:37:19 -0300

    feat: update CategoryController to use request/response models

--- src/main/java/wtf/tonho/movieflix/controller/CategoryController.java
+++ src/main/java/wtf/tonho/movieflix/controller/CategoryController.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import wtf.tonho.movieflix.CategoryMapper.CategoryMapper;
+import wtf.tonho.movieflix.controller.request.CategoryRequest;
+import wtf.tonho.movieflix.controller.response.CategoryResponse;
 import wtf.tonho.movieflix.entity.Category;
 import wtf.tonho.movieflix.repository.CategoryRepository;
 import wtf.tonho.movieflix.service.CategoryService;
@@ -21,17 +24,20 @@ public class CategoryController {
 
 
     @GetMapping()
-    public List<Category> getAllCategories() {
-        return categoryService.findAll();
+    public List<CategoryResponse> getAllCategories() {
+        List<Category> categories = categoryService.findAll();
+       return categories.stream()
+               .map(CategoryMapper::toCategoryResponse)
+               .toList();
     }
 
     @PostMapping()
-    public Category saveCategory(@RequestBody Category category) {
-        return categoryService.saveCategory(category);
+    public CategoryResponse saveCategory(@RequestBody CategoryRequest request) {
+        CategoryMapper.toCategory(request)
     }
 
     @GetMapping("/{id}")
-    public Category getByCategory(@PathVariable Long id) {
+    public CategoryResponse getByCategory(@PathVariable Long id) {
        Optional<Category> category = categoryService.findById(id);
        if(category.isPresent()) {
            return category.get();
